b60003e4497ec08a7ab66a00804314388b756957
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-24 21:03:59 +1000

    commit tree ddd8e53c62bafa0cd4573bda7cb87caf65277392

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(username, userEmail){
+    return fetchUsers(username, userEmail);
+}
+
+function fetchUsers(username, userEmail){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(userData => {
+        document.body.innerHTML = userData.id;
+    })
+    .catch(err => {
+        document.body.innerHTML = err.message;
+    })
+}
